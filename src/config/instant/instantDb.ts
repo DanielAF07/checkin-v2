b078381ea5a95fd681bf82ff17eea8b7b324d9f9
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2025-07-14 10:35:01 -0500

    chore: remove console log for APP_ID in instantDb configuration

--- src/config/instant/instantDb.ts
+++ src/config/instant/instantDb.ts
@@ -4,5 +4,4 @@ import schema from '../../../instant.schema';
 // ID for app: asistencia
 const APP_ID = process.env.EXPO_PUBLIC_INSTANT_APP_ID || '';
 
-console.log(APP_ID);
 export const instantDb = init({ appId: APP_ID, schema: schema });
